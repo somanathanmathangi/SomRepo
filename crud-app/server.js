dd92d5e2f88934bb312d31697bfbb9106e89be0f
som <somanathan_c@yahoo.com>
2026-05-09 18:04:28 +0530

    commit 18 04

--- crud-app/server.js
+++ crud-app/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const crypto = require('crypto');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
